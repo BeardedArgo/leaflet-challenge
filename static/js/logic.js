78f486277c76959469dd1099b0bdaba88c25684d
BeardedArgo <77208185+BeardedArgo@users.noreply.github.com>
2021-05-29 09:18:29 -0500

    hi

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,16 +2,18 @@ console.log("working");
 
 var apiKey = "pk.eyJ1IjoiYmVhcmRlZGFyZ28iLCJhIjoiY2tva284ZG1kMDFjbTJvbW95ODF1dHJobSJ9.O5V-b9cVVWtTXpydX6u_HQ"
 
-var gray = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: apiKey
-});
+var gray = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 3,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+})
 
 var map = L.map("mapid", {
   center: [
-    41.7, -94.5
+    40.7, -94.5
   ],
   zoom: 3
 });
